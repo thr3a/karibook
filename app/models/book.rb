f193a3a92f5b13831af3e6a41d201b12c5722e02
thr3a <12jk172@ms.dendai.ac.jp>
2015-12-01 09:09:28 +0900

    User/Bookモデルにバリデーションを設けた

--- app/models/book.rb
+++ app/models/book.rb
@@ -1,3 +1,9 @@
 class Book < ActiveRecord::Base
   has_one :kashidashi, foreign_key: :isbn, dependent: :destroy
+
+  validates :isbn, presence: true, uniqueness: true, length: { is: 13 }
+  validates :name, presence: true, uniqueness: true
+  validates :author, presence: true
+  validates :publisher, presence: true
+  validates :published_date, presence: true
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,3 +1,5 @@
 class User < ActiveRecord::Base
   has_one :kashidashi, dependent: :destroy
+  validates :name, presence: true
+  validates :id, presence: true, uniqueness: true
 end
