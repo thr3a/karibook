3780915847b5bcd01fb8d7b6826e718b4e99b98b
thr3a <12jk172@ms.dendai.ac.jp>
2015-12-01 09:56:20 +0900

    Bookモデルに編集/削除機能追加

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,4 +1,5 @@
 class BooksController < ApplicationController
+  before_action :set_book, only: [:edit, :update, :destroy]
 
   # GET /books
   def new
@@ -14,10 +15,31 @@ class BooksController < ApplicationController
       render :new
     end
   end
+  # GET /books/:isbn/edit
+  def edit
+  end
+  # PATCH/PUT /books/:isbn
+  def update
+    if @book.update(book_params)
+      redirect_to root_path, notice: '編集に成功しました'
+    else
+      render :edit
+    end
+  end
+
+  # DELETE /books/:isbn
+  def destroy
+    @book.destroy
+    redirect_to root_path, notice: '削除に成功しました'
+  end
 
   private
     # Only allow a trusted parameter "white list" through.
     def book_params
       params.require(:book).permit(:isbn, :name, :author, :publisher, :published_date)
+    end
+
+    def set_book
+      @book = Book.find(params[:id])
     end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
   root 'kashidasis#index'
-  resources :books , only: [:new, :create]
-  resources :users , only: [:new, :create]
+  resources :books , except: :show
+  resources :users , except: :show
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
